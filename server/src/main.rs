c790b8ebf77635e9e3dc470c3c6408acb1f3eb58
Victor Porton <porton.victor@gmail.com>
2022-06-12 07:46:19 +0300

    server

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ use env_logger::TimestampPrecision;
 use clap::Parser;
 use lambda_web::{is_running_on_lambda, LambdaError, run_actix_on_lambda};
 use crate::our_db_pool::{db_pool_builder, MyPool, MyDBConnectionCustomizer, MyDBConnectionManager};
-use crate::pages::{about_us, not_found};
+use crate::pages::{about_us, initiate_payment, not_found};
 
 mod our_db_pool;
 mod pages;
@@ -64,6 +64,7 @@ async fn main() -> Result<(), LambdaError> {
         .app_data(Data::new(config2.clone()))
         .app_data(Data::new(common.clone()))
         .service(about_us)
+        .service(initiate_payment)
         .service(
             actix_files::Files::new("/media", "media").use_last_modified(true),
         )
