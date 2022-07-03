a6f02ebcb2f95a246bd0994687034409749d443c
Victor Porton <porton.victor@gmail.com>
2022-07-03 20:13:15 +0300

    bug fix

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,5 +1,9 @@
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./src/App";
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+// const app = document.getElementById("app");
+// ReactDOM.render(<App />, app);
+
+const root = createRoot(document.getElementById("app"));
+root.render(<App />);
