7f0b8fa0a100c0e4c65dfcb58604b1d4479df6cf
Victor Porton <porton.victor@gmail.com>
2022-07-03 20:06:55 +0300

    working on card payments

--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -1,32 +1,55 @@
-import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk';
+import { Elements, PaymentElement } from "@stripe/react-stripe-js";
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+import { useEffect, useRef, useState } from "react";
+import { backendUrlPrefix } from "../config";
 
 export default function Card() {
+    const userAccount = useRef(null);
     return <>
         <p>You mine CardToken by using a credit card or a bank account (unlike Bitcoin that is mined by costly equipment).</p>
         <p>To mine an amount of CardToken corresponding to a certain amount of money, pay any amount of money
-            to your account <input type="text" id="userAccount"/>
-            by <button onClick={initiatePayment}>clicking this button</button>
+            to your account <input type="text" id="userAccount" ref={userAccount}/>
             first your account will be anonymously stored in our database and then you pay.
             After you paid, our system will initiate money transfer to your account.
             <strong>You must pay during 12 hours since clicking the button. If you pay late,
                 your payment may be stalled in a crypto account with no one being able to get the money.</strong>
+            <PaymentForm/>
         </p>
     </>
 }
 
-function doInitiatePayment(userAddress) {
-    new RampInstantSDK({
-        hostAppName: 'CardToken Credit Card Mine',
-        hostLogoUrl: 'https://yourdapp.com/yourlogo.png',
-        //   swapAmount: '150000000000000000000', // 150 ETH in wei
-        swapAsset: 'DOT',
-        fiatCurrency: 'USD',
-        userAddress: '0x0', // FIXME
-        //   hostApiKey: // FIXME
-    }).on('*', event => console.log(event)).show();
+function PaymentForm(userAddress) {
+    const [options, setOptions] = useState(null as unknown as object);
+    const [stripePromise, setStripePromise] = useState(null as Promise<Stripe | null> | null);
+    useEffect(() => {
+        async function doIt() {
+            const stripe_pubkey = await (await fetch(backendUrlPrefix + "/stripe-pubkey")).text();
+            const res = await (await fetch(backendUrlPrefix + "/create-payment-intent")).json();
+            const client_secret: string = res["client_secret"];
+
+            const stripePromise_: Promise<Stripe | null> = loadStripe(stripe_pubkey);
+
+            setOptions({
+                clientSecret: client_secret,
+                appearance: {},
+            });
+            setStripePromise(stripePromise_);
+        }
+        doIt();
+    }, []);
+
+    return (
+        <Elements stripe={stripePromise} options={options}>
+            <form>
+                <PaymentElement />
+                <button>Submit</button>
+            </form>
+        </Elements>
+    );
 }
 
-function initiatePayment() {
-    const userAddress = document.getElementById('userAccount');
-    doInitiatePayment(userAddress);
-}
+
+// async function initiatePayment() {
+//     const userAddress = document.getElementById('userAccount');
+//     await doInitiatePayment(userAddress);
+// }
