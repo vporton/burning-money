c7625c97e37541cc2e69f54a1d67e9dcc4f86266
Victor Porton <porton.victor@gmail.com>
2022-07-17 17:40:50 +0300

    AggregatorV3Interface

--- contracts/imports.sol
+++ contracts/imports.sol
@@ -3,3 +3,4 @@ import "mexa/contracts/6/forwarder/BiconomyForwarder.sol";
 import "mexa/contracts/6/forwarder/ERC20Forwarder.sol";
 import "mexa/contracts/6/forwarder/ERC20ForwarderProxy.sol";
 import "mexa/contracts/6/feeManager/CentralisedFeeManager.sol";
+import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
