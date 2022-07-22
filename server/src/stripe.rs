93dcb28117e0910afa18dcba6e3296e9d6e8f5ae
Victor Porton <porton.victor@gmail.com>
2022-07-22 03:27:07 +0300

    removed commented out code

--- server/src/stripe.rs
+++ server/src/stripe.rs
@@ -25,42 +25,6 @@ pub struct CreateStripeCheckout {
     fiat_amount: f64,
 }
 
-// #[get("/create-stripe-checkout")]
-// pub async fn create_stripe_checkout(q: web::Query<CreateStripeCheckout>, common: web::Data<Common>) -> Result<impl Responder, MyError> {
-//     let client = Client::new(common.config.stripe.secret_key.clone());
-//
-//     let product = {
-//         let create_product = CreateProduct::new("Mining CardToken");
-//         Product::create(&client, create_product).await?
-//     };
-//
-//     let price = {
-//         let mut create_price = CreatePrice::new(Currency::USD);
-//         create_price.product = Some(IdOrCreate::Id(&product.id));
-//         create_price.unit_amount = Some((q.fiat_amount * 100.0) as i64);
-//         create_price.expand = &["product"];
-//         Price::create(&client, create_price).await?
-//     };
-//
-//     let mut params =
-//         CreateCheckoutSession::new("http://test.com/cancel", "http://test.com/success"); // FIXME
-//     // params.customer = Some(customer.id);
-//     params.mode = Some(CheckoutSessionMode::Payment);
-//     params.line_items = Some(vec![CreateCheckoutSessionLineItems {
-//         price: Some(price.id.to_string()),
-//         quantity: Some(1), // FIXME
-//         ..Default::default()
-//     }]);
-//     params.expand = &["line_items", "line_items.data.price.product"];
-//
-//     let session = CheckoutSession::create(&client, params).await?;
-//     if let Some(url) = session.url {
-//         Ok(HttpResponse::TemporaryRedirect().append_header((LOCATION, url)).body(""))
-//     } else {
-//         Ok(HttpResponse::Ok().body("Stripe didn't return a URL.")) // FIXME
-//     }
-// }
-
 #[get("/stripe-pubkey")]
 pub async fn stripe_public_key(readonly: web::Data<CommonReadonly>) -> impl Responder {
     HttpResponse::Ok().body(readonly.config.stripe.public_key.clone())
