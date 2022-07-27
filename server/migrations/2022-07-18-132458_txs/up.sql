c2f25eaa4fb82ab8d334d2c740aeb675de8a2ec4
Victor Porton <porton.victor@gmail.com>
2022-07-27 09:20:13 +0300

    tx_id NULL

--- server/migrations/2022-07-18-132458_txs/up.sql
+++ server/migrations/2022-07-18-132458_txs/up.sql
@@ -10,10 +10,11 @@ CREATE TABLE txs (
     crypto_amount BIGINT NOT NULL,
     bid_date BIGINT NOT NULL,
     status txs_status_type NOT NULL DEFAULT 'before_ordered',
-    tx_id BYTEA NOT NULL DEFAULT '', -- Ethereum tx ID. -- FIXME: Should be NULL.
+    tx_id BYTEA NULL,
     CONSTRAINT txs_user_fk FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT payment_intent_id_uniq UNIQUE(payment_intent_id)
 );
+--ALTER TABLE txs ADD CONSTRAINT payment_intent_id_uniq UNIQUE(payment_intent_id);
 CREATE INDEX txs_user ON txs USING HASH(user_id);
 CREATE INDEX txs_tx_id ON txs USING HASH(tx_id);
 CREATE INDEX txs_status ON txs USING BTREE(tx_id);
