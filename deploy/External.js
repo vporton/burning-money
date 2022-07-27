93e5600e19f41680f906eea9fc5c8b40a6205861
Victor Porton <porton.victor@gmail.com>
2022-07-27 05:49:33 +0300

    fund test ether

--- deploy/External.js
+++ deploy/External.js
@@ -5,6 +5,7 @@ const { myDeploy, updateAddress } = require('../lib/default-deployer');
 module.exports = async ({getNamedAccounts, deployments, network}) => {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
+    const [deployerSigner] = await ethers.getSigners();
     const networkName = hre.network.name;
     const addresses = JSON.parse(fs.readFileSync('addresses.json'))[network.name];
     if(addresses.collateralOracle) {
@@ -14,6 +15,12 @@ module.exports = async ({getNamedAccounts, deployments, network}) => {
         const testPriceOracle = await myDeploy(
             TestPriceOracle, network, deployer, "collateralOracle", [],
         );
+
+        const serverAccount = new ethers.Wallet('0x' + fs.readFileSync("server/ethereum.priv"));
+        deployerSigner.sendTransaction({
+            to: await serverAccount.getAddress(),
+            value: ethers.utils.parseEther("0.5"),
+        });
     }
 };
 module.exports.tags = ['External'];
