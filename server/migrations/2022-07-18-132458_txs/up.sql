02e9b6c09031e2a3931c28d3fb82ccaa55331553
Victor Porton <porton.victor@gmail.com>
2022-07-27 09:49:19 +0300

    removed a done FIXME comment

--- server/migrations/2022-07-18-132458_txs/up.sql
+++ server/migrations/2022-07-18-132458_txs/up.sql
@@ -1,4 +1,3 @@
--- FIXME: Add 'precreated' not to lose transactions if SIGKILL after finish_payment()?
 CREATE TYPE txs_status_type AS ENUM ('before_ordered', 'ordered', 'submitted_to_blockchain', 'confirmed');
 
 CREATE TABLE txs (
