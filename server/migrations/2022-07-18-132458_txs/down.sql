685be7bc92970ead3328eff40a5684634203a2e9
Victor Porton <porton.victor@gmail.com>
2022-07-29 07:47:27 +0300

    store transaction times

--- server/migrations/2022-07-18-132458_txs/down.sql
+++ server/migrations/2022-07-18-132458_txs/down.sql
@@ -1,2 +1,4 @@
+DROP TRIGGER txs_sync_lastmod ON txs;
+DROP FUNCTION txs_sync_lastmod;
 DROP TABLE txs;
 DROP TYPE txs_status_type;
--- server/migrations/2022-07-18-132458_txs/up.sql
+++ server/migrations/2022-07-18-132458_txs/up.sql
@@ -2,6 +2,7 @@ CREATE TYPE txs_status_type AS ENUM ('before_ordered', 'ordered', 'submitted_to_
 
 CREATE TABLE txs (
     id BIGSERIAL NOT NULL PRIMARY KEY,
+    last_modified TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     payment_intent_id TEXT NOT NULL,
     user_id BIGINT NOT NULL,
     eth_account BYTEA NOT NULL,
@@ -14,6 +15,21 @@ CREATE TABLE txs (
     CONSTRAINT payment_intent_id_uniq UNIQUE(payment_intent_id)
 );
 --ALTER TABLE txs ADD CONSTRAINT payment_intent_id_uniq UNIQUE(payment_intent_id);
+CREATE INDEX txs_last_modified ON txs USING BTREE(last_modified);
 CREATE INDEX txs_user ON txs USING HASH(user_id);
 CREATE INDEX txs_tx_id ON txs USING HASH(tx_id);
 CREATE INDEX txs_status ON txs USING BTREE(tx_id);
+
+CREATE FUNCTION txs_sync_lastmod() RETURNS trigger AS $$
+BEGIN
+  NEW.last_modified := NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER
+  txs_sync_lastmod
+BEFORE UPDATE ON
+  txs
+FOR EACH ROW EXECUTE PROCEDURE
+  txs_sync_lastmod();
